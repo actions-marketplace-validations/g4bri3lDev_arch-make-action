8ff4379f12418008f65c7ec01609e299bd5e5fec
g4bri3lDev <g.lackermeier@gmx.de>
2020-07-29 21:15:52 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,11 @@
 if [ -d "/github" ]; then
 sudo chown -R build /github/workspace /github/home
 fi
-git clone https://aur.archlinux.org/yay.git
-cd yay
-echo -e "y\ny" | makepkg -si
-cd ..
-rm -rf yay
+#git clone https://aur.archlinux.org/yay.git
+#cd yay
+#echo -e "y\ny" | makepkg -si
+#cd ..
+#rm -rf yay
 
 make clean
 make
